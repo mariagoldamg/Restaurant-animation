4b27bf14701eb6f96910466b470758ee42e07559
mariagoldamg <mariamelnikova88@gmail.com>
2023-04-11 13:43:53 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+/*gsap.to('.one', { duration: 1, delay: .8, x: '-100vw', ease: 'power2.in', opacity: 0 })
+gsap.from('.two', { duration: 2, delay: .9, x: '-100vw', ease: 'power2.in', opacity: 0 })
+gsap.from('.myImage', { duration: 2, delay: 1, y: '10vw', ease: 'power2.in', opacity: 0 })
+gsap.from('.header', { duration: 2, delay: 1.6, y: '10vw', ease: 'power2.in', opacity: 0 })
+gsap.from('.links', { duration: 2, delay: 2, y: '10vw', ease: 'power2.in', opacity: 0 })
+gsap.from('.mobile', { duration: 2, delay: 2, y: '10vw', ease: 'power2.in', opacity: 0 })
+
+gsap.from('h1', { duration: 2, delay: 1.5, x: '-100vw', ease: 'power2.in', opacity: 0 })
+
+
+gsap.fromTo('.btn', { opacity: 0, scale: 0, rotation: 720 }, { duration: 1, delay: 3.8, opacity: 1, scale: 1, rotation: 0 })*/
+
+gsap.to('.one', { duration: 1, delay: .8, y: '-400', ease: 'power2.inOut', opacity: 0 });
+gsap.to('.two', { duration: 2, delay: .8, y: '1000', ease: 'power2.inOut', opacity: 0 });
+gsap.from('.header', { duration: 2, delay: 1.6, y:'300', ease: 'back', opacity: 0});
+gsap.from('.links', { duration: 2, delay: 1.6, y: '400', ease: 'back', opacity: 0 })
+gsap.from('.mobile', { duration: 2, delay: 1.6, y: '300', ease: 'back', opacity: 0 })
+
+gsap.from('.btn', { duration: 3, delay: 2, opacity: 0,  rotation: 0 })
